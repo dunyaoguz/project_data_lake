94929e2eb4e719d2184686a24ea9f15617e5d5c8
Dunya <dunya@RenoRuns-MacBook-Pro-3.local>
2020-02-13 20:20:17 -0500

    complete song data etl

--- etl.py
+++ etl.py
@@ -11,6 +11,9 @@ aws_access = os.environ['AWS_ACCESS_KEY_ID']
 aws_secret_access = os.environ['AWS_SECRET_ACCESS_KEY']
 
 def create_spark_session():
+    """
+    Begin a spark session
+    """
     spark = SparkSession \
         .builder \
         .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
@@ -18,48 +21,51 @@ def create_spark_session():
     return spark
 
 def process_song_data(spark, input_data, output_data):
+    """
+    Extract data on unique songs and artists from the source data source and load onto the target data source
+    """
     # get filepath to song data file
-    song_data =
+    song_data = os.path.join(input_data, 'song_data/A/A/A/*.json')
 
     # read song data file
-    df =
+    df = spark.read.json(song_data)
 
     # extract columns to create songs table
-    songs_table =
+    songs_table = df[['song_id', 'title', 'artist_id', 'year', 'duration']].dropDuplicates(['song_id'])
 
     # write songs table to parquet files partitioned by year and artist
-    songs_table
+    songs_table.write.partitionBy('year', 'artist_id').parquet(os.path.join(output_data, 'songs.parquet'), 'overwrite')
 
     # extract columns to create artists table
-    artists_table =
+    artists_table = df['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude'].dropDuplicates(['artist_id'])
 
     # write artists table to parquet files
-    artists_table
+    artists_table.write.parquet(os.path.join(output_data, 'artists.parquet'), 'overwrite')
 
 
 def process_log_data(spark, input_data, output_data):
     # get filepath to log data file
-    log_data =
+    log_data = os.path.join(input_data,"log_data/*/*/*.json")
 
     # read log data file
-    df =
+    df = spark.read.json(log_data)
 
     # filter by actions for song plays
-    df =
+    df = df['ts', 'userId', 'level','sessionId', 'location', 'userAgent']
 
     # extract columns for users table
-    artists_table =
+    users_table = df['userId', 'firstName', 'lastName', 'gender', 'level'].dropDuplicates(['user_id'])
 
     # write users table to parquet files
-    artists_table
+    users_table.write.parquet(os.path.join(output_data, 'users.parquet'), 'overwrite')
 
     # create timestamp column from original timestamp column
-    get_timestamp = udf()
-    df =
+    get_timestamp = udf(lambda x: str(int(int(x)/1000)))
+    df = df.withColumn('timestamp', get_timestamp(df.ts))
 
     # create datetime column from original timestamp column
-    get_datetime = udf()
-    df =
+    get_datetime = udf(lambda x: str(datetime.fromtimestamp(int(x) / 1000.0)))
+    df = df.withColumn("datetime", get_datetime(df.ts))
 
     # extract columns to create time table
     time_table =
